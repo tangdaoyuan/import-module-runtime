6da3d42d5aea40b3ba55d16811734677d29246cc
唐道源 <tangdaoyuan@megvii.com>
2022-08-26 18:13:49 +0800

    chore: debug

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export function findNodeModule(modulePath: string, _options?: Options): NodeModu
   const fileName = path.basename(mockFilePath)
 
   try {
+    // eslint-disable-next-line no-console
+    console.log(mockFilePath)
     const rawCjsCode = transform(mockFilePath)
     const mod = new _module(mockFilePath) as ExposeNodeModule
     mod.filename = fileName
